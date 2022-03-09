16f8440ca821111915a6b57403a5c0a760677486
kora gado malick <kmalick166@yahoo.com>
2022-03-09 15:02:13 +0100

    firstcommit

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php 
+$con= new mysqli('localhost','root','','etrilabs');
+if(isset($_GET['deleteid'])){
+        $id=$_GET['deleteid'];
+
+        $sql="delete from nomexolabs where id=$id";
+        $resultat=mysqli_query($con,$sql);
+        if($resultat){
+           echo  'suppression effectué avec succès';
+        }  
+           else{
+            die(mysql_error($con));
+        }
+}
+
+?>
